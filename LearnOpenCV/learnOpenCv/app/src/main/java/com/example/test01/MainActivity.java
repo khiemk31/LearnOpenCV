3a0d514441f79395c2bc13ef98943a3ba24ea9e2
khiemk31 <khiemk31@gmail.com>
2021-10-19 15:30:46 +0700

    Learn2

--- LearnOpenCV/learnOpenCv/app/src/main/java/com/example/test01/MainActivity.java
+++ LearnOpenCV/learnOpenCv/app/src/main/java/com/example/test01/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.test01;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -7,10 +8,18 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import org.opencv.android.BaseLoaderCallback;
@@ -18,99 +27,73 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCamera2View;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 
-public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
-    // Mat : được gọi là "ma trận" trong Toán Học  - một hình chữ nhật có đại lượng được quy định bởi các
-    // hàng và cột (rows, colums) . Những đại lượng đó đc đại diện cho pixel trong TH nó là Hình Ảnh
-    //VD : mọi phần tử của ma trận có thể là màu của mỗi pixel Mat Hình Ảnh
-    //Dưới đây là khai báo biến kiểu DL MAT nhưng chưa khởi tạo dữ liệu
-    Mat mat1, mat2, mat3;
-    CameraBridgeViewBase cameraBridgeViewBase;
-    BaseLoaderCallback baseLoaderCallback;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView img1;
+    Uri imageUri;
+    Bitmap grayBitmap, imageBitmap;
+    Button btnGallery, btnGray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cameraBridgeViewBase = (JavaCamera2View) findViewById(R.id.my_camera);
-        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
-        cameraBridgeViewBase.setCvCameraViewListener(this);
+        OpenCVLoader.initDebug();
+        img1 = (ImageView) findViewById(R.id.img_test);
+        btnGallery = findViewById(R.id.btn_gallery);
+        btnGray = findViewById(R.id.btn_gray);
+        btnGallery.setOnClickListener(v -> openGallery());
+        btnGray.setOnClickListener(v -> convertToGray());
 
-        baseLoaderCallback = new BaseLoaderCallback(this) {
-            @Override
-            public void onManagerConnected(int status) {
-                switch (status) {
-                    case BaseLoaderCallback.SUCCESS:
-                        cameraBridgeViewBase.enableView();
-                        break;
-                    default:
-                        super.onManagerConnected(status);
-                        break;
-                }
+    }
+
+    public void openGallery() {
+        Intent myIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(myIntent, 100);
+    }
+
+    public void convertToGray() {
+        Mat Rgba = new Mat();
+        Mat grayMat = new Mat();
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inDither = false;
+        options.inSampleSize = 4;
+
+        int width = imageBitmap.getWidth();
+        int heght = imageBitmap.getHeight();
+
+        grayBitmap = Bitmap.createBitmap(width, heght, Bitmap.Config.RGB_565);
+
+        //bitmap -> MAT:
+        Utils.bitmapToMat(imageBitmap, Rgba);
+        Imgproc.cvtColor(Rgba, grayMat, Imgproc.COLOR_RGB2GRAY);
+        //Mat -> bitmap :
+        Utils.matToBitmap(grayMat, grayBitmap);
+
+        img1.setImageBitmap(grayBitmap);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
+            Uri imageUri = data.getData();
+            try {
+                imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        };
-    }
+            img1.setImageBitmap(imageBitmap);
 
-    public static void checkCameraPermissions(Context context) {
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted
-            Log.d("checkCameraPermissions", "No Camera Permissions");
-            ActivityCompat.requestPermissions((Activity) context,
-                    new String[]{Manifest.permission.CAMERA},
-                    100);
         }
     }
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-        mat1 = new Mat(width, height, CvType.CV_8UC4);
-        mat2 = new Mat(width, height, CvType.CV_8UC4);
-        mat3 = new Mat(width, height, CvType.CV_8UC4);
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-        mat1.release();
-    }
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        mat1 = inputFrame.rgba();
-        //Xoay frame 90 do:
-        Core.transpose(mat1, mat2);
-        Imgproc.resize(mat2, mat3,mat3.size(),0,0,0);
-        Core.flip(mat2, mat1, 1);
-        return mat1;
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (cameraBridgeViewBase != null) {
-            cameraBridgeViewBase.disableView();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (!OpenCVLoader.initDebug()) {
-            Toast.makeText(getApplicationContext(), "there is problem in Opencv", Toast.LENGTH_LONG).show();
-        } else {
-            baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
-        }
-        checkCameraPermissions(this);
-    }
-
-
 }
